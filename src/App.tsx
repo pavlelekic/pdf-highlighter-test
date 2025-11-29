0ab0c8690441292a1241fa7e5fe18a1509b8a24c
Pavle Lekic <7754193+pavlelekic@users.noreply.github.com>
2025-11-29 17:24:28 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
-import { PdfHighlighter, Highlight } from "react-pdf-highlighter";
+import { PdfHighlighter, Highlight, PdfLoader } from "react-pdf-highlighter";
 import { usePdfSearch } from "./hooks/usePdfSearch";
 import "react-pdf-highlighter/dist/style.css";
+
+interface SearchHighlight {
+  id: string;
+  text: string;
+  positions: Array<{
+    pageNumber: number;
+    rects: Array<{
+      x: number;
+      y: number;
+      width: number;
+      height: number;
+    }>;
+  }>;
+}
 
 export default function PdfDemo() {
   const { highlights, findHighlights } = usePdfSearch();
@@ -17,19 +31,46 @@ export default function PdfDemo() {
         Search
       </button>
       <div style={{ position: "relative", height: "100vh" }}>
-        <PdfHighlighter
-          pdfDocument="/sdi.pdf"
-          highlights={highlights}
-          highlightTransform={(h) => (
-            <Highlight
-              key={h.id}
-              position={{
-                pageNumber: h.positions[0].pageNumber,
-                rects: h.positions.flatMap((p) => p.rects),
-              }}
+        <PdfLoader url="/sdi.pdf" beforeLoad={<div>Loading PDF...</div>}>
+          {(pdfDocument) => (
+            <PdfHighlighter
+              pdfDocument={pdfDocument}
+              highlights={highlights}
+              onScrollChange={() => {}}
+              scrollRef={() => {}}
+              onSelectionFinished={() => null}
+              enableAreaSelection={() => false}
+              highlightTransform={(h: SearchHighlight) => (
+                <Highlight
+                  key={h.id}
+                  isScrolledTo={false}
+                  position={{
+                    boundingRect: {
+                      left: h.positions[0].rects[0].x,
+                      top: h.positions[0].rects[0].y,
+                      width: h.positions[0].rects[0].width,
+                      height: h.positions[0].rects[0].height,
+                      pageNumber: h.positions[0].pageNumber,
+                    },
+                    rects: h.positions.flatMap((p) =>
+                      p.rects.map((r) => ({
+                        left: r.x,
+                        top: r.y,
+                        width: r.width,
+                        height: r.height,
+                        pageNumber: p.pageNumber,
+                      })),
+                    ),
+                  }}
+                  comment={{
+                    text: h.text,
+                    emoji: "",
+                  }}
+                />
+              )}
             />
           )}
-        />
+        </PdfLoader>
       </div>
     </div>
   );
